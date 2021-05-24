5f443864e5e0c740ee7a06b0f0296ac1395627fc
potrijo <backinmarc@gmail.com>
2021-05-24 09:36:17 +0200

    Marc Commit

--- eclipse-workspaceJavaFX/NeckoTrastos/src/necko/Main.java
+++ eclipse-workspaceJavaFX/NeckoTrastos/src/necko/Main.java
@@ -1,27 +1,5 @@
 package necko;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 
-public class Main extends Application {
-	@Override
-	// comentario
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-}
+import javafx.application.;
